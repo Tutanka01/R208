4ddd6cb4b4e6afa5afff05602353cfd9e5c7c4e5
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-03-29 06:33:04 +0000

    io

--- SUJET2/main.py
+++ SUJET2/main.py
@@ -25,31 +25,44 @@ def ascendants(tab, num):
         if tab[i][1] == num:
             ascendants.append(tab[i][0])
     return ascendants
-#6 
+#6 Idem mais pour les descendants d'une personne cette fois.
 def descendants(tab, num):
     descendants = []
     for i in range(len(tab)):
         if tab[i][0] == num:
             descendants.append(tab[i][1])
     return descendants
-#7
+
+#7 Idem pour les frères et soeurs d'une personne donnée.
 def freres_soeurs(tab, num):
+    # Create an empty list
     freres_soeurs = []
+    # For each element in the list
     for i in range(len(tab)):
+        # If the first element of the element is equal to num
         if tab[i][0] == num:
+            # Append the second element of the element to the list
             freres_soeurs.append(tab[i][1])
+    # Return the list
     return freres_soeurs
 #8
+
+
 def tri(tab):
-    for i in range(len(tab)):
-        for j in range(len(tab)):
-            if tab[i][0] < tab[j][0]:
-                tab[i], tab[j] = tab[j], tab[i]
-            elif tab[i][0] == tab[j][0]:
-                if tab[i][1] < tab[j][1]:
-                    tab[i], tab[j] = tab[j], tab[i]
-    return tab
+    for i in range(len(tab)): #Pour chaque personne de la liste
+        for j in range(len(tab)): #On compare la personne à toutes les autres personnes de la liste
+            if tab[i][0] < tab[j][0]: #Si le nom de la personne i est avant le nom de la personne j
+                tab[i], tab[j] = tab[j], tab[i] #On inverse les deux personnes
+            elif tab[i][0] == tab[j][0]: #Si les noms sont les mêmes
+                if tab[i][1] < tab[j][1]: #On compare les prénoms
+                    tab[i], tab[j] = tab[j], tab[i] #On inverse les deux personnes
+    les_personnes = tab
+    return affichage(les_personnes)
 #9 
+'''Dans cette fonction on va comparer les années de naissance des personnes, puis les mois et enfin les jours.
+pour cela on va utiliser les indices 2, 1 et 0 de la liste date, on va consulter une par une les dates de chaque 
+personne pour voir qui a le jour le moins eleve, puis le mois le moins eleve et enfin l'annee la moins elevee.'''
+
 def tri_age(tab):
     for i in range(len(tab)):
         for j in range(len(tab)):
@@ -61,7 +74,8 @@ def tri_age(tab):
                 elif tab[i][3][1] == tab[j][3][1]:
                     if tab[i][3][0] < tab[j][3][0]:
                         tab[i], tab[j] = tab[j], tab[i]
-    return tab
+    les_personnes = tab
+    return affichage(les_personnes)
 
 def menu():
     print("1. Ajouter une personne")
@@ -88,56 +102,69 @@ def main():
             date = date.split("/")
             date = [int(date[0]), int(date[1]), int(date[2])]
             ajout(les_personnes, nom, prenom, sexe, date)
+            print('\n')
+            
         elif choix == 2:
             affichage(les_personnes)
+        
         elif choix == 3:
             nom = input("Nom : ")
             prenom = input("Prénom : ")
             print(num_personne(les_personnes, nom, prenom))
+            print('\n')
+        
         elif choix == 4:
             nom1 = input("Nom de la première personne : ")
             prenom1 = input("Prénom de la première personne : ")
             nom2 = input("Nom de la deuxième personne : ")
             prenom2 = input("Prénom de la deuxième personne : ")
             ajout_lien(lien_parente, num_personne(les_personnes, nom1, prenom1), num_personne(les_personnes, nom2, prenom2))
+        
         elif choix == 5:
             nom = input("Nom : ")
             prenom = input("Prénom : ")
             print(ascendants(lien_parente, num_personne(les_personnes, nom, prenom)))
+        
         elif choix == 6:
             nom = input("Nom : ")
             prenom = input("Prénom : ")
             print(descendants(lien_parente, num_personne(les_personnes, nom, prenom)))
+       
         elif choix == 7:
             nom = input("Nom : ")
             prenom = input("Prénom : ")
             print(freres_soeurs(lien_parente, num_personne(les_personnes, nom, prenom)))
+        
         elif choix == 8:
             print(tri(les_personnes))
+        
         elif choix == 9:
             print(tri_age(les_personnes))
+        
         elif choix == 10:
             break
+        
         else:
             print("Erreur de saisie")
 
 
 
 ajout(les_personnes, "DUPONT", "Jean", "M", [12, 5, 1980])
-ajout(les_personnes, "Yoan", "Croisier", "F", [26, 12, 2003])
+ajout(les_personnes, "Sophie", "Metatidj", "F", [26, 12, 2003])
 ajout(les_personnes, "El Akhal", "Mohamad", "M", [12, 5, 2003])
 ajout(les_personnes, "Noah", "Clergaud-Metraud", "M", [31, 6, 2010])
 ajout(les_personnes, "Léa", "Clergaud-Metraud", "F", [25, 4 , 2010])
 ajout(les_personnes, "Wafa", "Bhamza", "F", [26, 4 , 2010])
 ajout(les_personnes, "lucas", "modric", "F", [27, 4 , 2010])
 
-ajout_lien(lien_parente, num_personne(les_personnes, "DUPONT", "Jean"), num_personne(les_personnes, "Yoan", "Croisier"))
+ajout_lien(lien_parente, num_personne(les_personnes, "DUPONT", "Jean"), num_personne(les_personnes, "Sophie", "Metatidj"))
 ajout_lien(lien_parente, num_personne(les_personnes, "DUPONT", "Jean"), num_personne(les_personnes, "El Akhal", "Mohamad"))
-ajout_lien(lien_parente, num_personne(les_personnes, "Yoan", "Croisier"), num_personne(les_personnes, "El Akhal", "Mohamad"))
+ajout_lien(lien_parente, num_personne(les_personnes, "Sophie", "Metatidj"), num_personne(les_personnes, "El Akhal", "Mohamad"))
 ajout_lien(lien_parente, num_personne(les_personnes, "El Akhal", "Mohamad"), num_personne(les_personnes, "Noah", "Clergaud-Metraud"))
 ajout_lien(lien_parente, num_personne(les_personnes, "El Akhal", "Mohamad"), num_personne(les_personnes, "Léa", "Clergaud-Metraud"))
-ajout_lien(lien_parente, num_personne(les_personnes, "Yoan", "Croisier"), num_personne(les_personnes, "Lea", "Clergaud-Metraud"))
-ajout_lien(lien_parente, num_personne(les_personnes, "Yoan", "Croisier"), num_personne(les_personnes, "Wafa", "Bhamza"))
+ajout_lien(lien_parente, num_personne(les_personnes, "Sophie", "Metatidj"), num_personne(les_personnes, "Lea", "Clergaud-Metraud"))
+ajout_lien(lien_parente, num_personne(les_personnes, "Sophie", "Metatidj"), num_personne(les_personnes, "Wafa", "Bhamza"))
 ajout_lien(lien_parente, num_personne(les_personnes, "Wafa", "Bhamza"), num_personne(les_personnes, "lucas", "modric"))
 
-menu()
+
+main()
